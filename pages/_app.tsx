00e9c8b060c2bcfd192133c48a69974362d75848
Bennett Dams <dams.bennett@gmail.com>
2021-04-12 01:10:46 +0200

    Integrate TailwindCSS

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+
+function App({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />
+}
+export default App
