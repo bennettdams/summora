17331a062d4abfba7765c3d68570ae499f7dab4c
Bennett Dams <dams.bennett@gmail.com>
2021-04-12 02:26:45 +0200

    Integrate Prisma

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,91 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import { Post, Prisma, PrismaClient } from '@prisma/client'
+import { useState } from 'react'
+const prisma = new PrismaClient()
 
-export const Home = (): JSX.Element => (
-  <div className="min-h-screen flex flex-col items-center justify-center bg-lime-50">
-    <Head>
-      <title>Condu</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+export const getServerSideProps: GetServerSideProps = async () => {
+  const posts = await prisma.post.findMany()
+  return {
+    props: {
+      initialPosts: posts,
+    },
+  }
+}
 
-    <header className="h-12 w-full top-0 fixed bg-lime-500">Condu</header>
+async function savePost(post: Prisma.PostCreateInput) {
+  const response = await fetch('/api/posts', {
+    method: 'POST',
+    body: JSON.stringify(post),
+  })
 
-    <main className="flex-1 mt-20 flex flex-col">
-      <h1 className="bg-red-200">Welcome to Condu!</h1>
-    </main>
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
 
-    <footer className="w-full h-24 border-t flex items-center justify-center">
-      <p>Condun</p>
-    </footer>
+  return await response.json()
+}
 
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
+export const Home = ({
+  initialPosts,
+}: {
+  initialPosts: Post[]
+}): JSX.Element => {
+  const [posts] = useState<Post[]>(initialPosts)
 
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-)
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-lime-50">
+      <Head>
+        <title>Condu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className="h-12 w-full top-0 fixed bg-lime-500">Condu</header>
+
+      <main className="flex-1 mt-20 flex flex-col">
+        <h1 className="bg-red-200">Welcome to Condu!</h1>
+        <button
+          onClick={async () => {
+            try {
+              const result = await savePost({
+                title: 'title 2',
+                content: 'content',
+              })
+              console.log(result)
+            } catch (err) {
+              console.log(err)
+            }
+          }}
+        >
+          New
+        </button>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </main>
+
+      <footer className="w-full h-24 border-t flex items-center justify-center">
+        <p>Condun</p>
+      </footer>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export default Home
