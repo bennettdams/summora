01ad1bac89eb72c94617f8dbab0538fc5c85e377
Bennett Dams <dams.bennett@gmail.com>
2022-09-06 22:09:20 +0200

    Reusable field error component | Optimize update schema

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,5 +1,12 @@
 import { forwardRef, ReactNode } from 'react'
-import { Control, Controller, FieldPath, FieldValues } from 'react-hook-form'
+import {
+  Control,
+  Controller,
+  FieldErrors,
+  FieldPath,
+  FieldValues,
+} from 'react-hook-form'
+import { generalFormErrorKey } from '../lib/schemas'
 import { Button, ButtonProps } from './Button'
 import { DropdownItem, DropdownSelect } from './DropdownSelect'
 import { IconOkCircle } from './Icon'
@@ -20,26 +27,33 @@ export function FormLabel({
 export type FormSubmitProps = {
   isLoading: boolean
   isValid: boolean
+  isDirty: boolean
   isValidating: boolean
   isSubmitted: boolean
   isSubmitting: boolean
   icon?: ButtonProps['icon']
+  /** By default, we allow submitting initially without changes to trigger validation. This can be disabled herewith. */
+  isInitiallySubmittable?: boolean
   children?: ReactNode
 }
 
 export function FormSubmit(props: FormSubmitProps): JSX.Element {
   // we ignore the "invalid" status for a form that has not been submitted yet
-  const isValidForm =
-    !props.isSubmitted || (props.isValid && !props.isValidating)
+  const isValidForm = props.isValid && !props.isValidating
+
+  const isEnabled =
+    isValidForm &&
+    !props.isSubmitting &&
+    (!!props.isDirty || props.isInitiallySubmittable)
 
   return (
     <Button
-      disabled={!isValidForm || props.isSubmitting}
+      disabled={!isEnabled}
       isSubmit={true}
       icon={props.icon ?? <IconOkCircle />}
       showLoading={props.isLoading}
     >
-      <span>{props.children ?? 'Submit'}</span>
+      <span>{props.children ?? 'Save'}</span>
       <input className="hidden" type="submit" />
     </Button>
   )
@@ -99,15 +113,30 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
   )
 })
 
-export function FormError({
-  message,
-}: {
-  message: string | undefined
-}): JSX.Element {
+function FormError({ message }: { message: string | undefined }): JSX.Element {
   return (
     <p className="text-left text-sm text-yellow-500">
       {message ?? <span className="opacity-0">&nbsp;</span>}
     </p>
+  )
+}
+
+export function FormFieldError<TFieldValues extends FieldValues>({
+  fieldName,
+  errors,
+  noMargin,
+}: {
+  fieldName: keyof FieldErrors<TFieldValues> | typeof generalFormErrorKey
+  errors: FieldErrors<TFieldValues>
+  noMargin?: boolean
+}): JSX.Element {
+  const errorForField = errors[fieldName]
+  return (
+    <div className={!noMargin ? 'mt-12' : ''}>
+      <p key={fieldName + ''} className="text-center text-yellow-500">
+        {errorForField?.message ?? <span className="opacity-0">&nbsp;</span>}
+      </p>
+    </div>
   )
 }
 
@@ -126,26 +155,34 @@ export function FormSelect<TFieldValues extends FieldValues>({
   name,
   items,
   unselectedLabel,
+  validationErrorMessage,
 }: {
   control: Control<TFieldValues>
   name: FieldPath<TFieldValues>
   items: DropdownItem[]
   unselectedLabel: string
+  validationErrorMessage?: string
 }): JSX.Element {
   return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ field: fieldSelection }) => (
-        <DropdownSelect
-          unselectedLabel={unselectedLabel}
-          selectedItemIdExternal={fieldSelection.value ?? null}
-          items={items}
-          onChangeSelection={(selectedItemId) => {
-            fieldSelection.onChange(selectedItemId)
-          }}
-        />
-      )}
-    />
+    <div className="relative">
+      <Controller
+        name={name}
+        control={control}
+        render={({ field: fieldSelection }) => (
+          <DropdownSelect
+            unselectedLabel={unselectedLabel}
+            selectedItemIdExternal={fieldSelection.value ?? null}
+            items={items}
+            onChangeSelection={(selectedItemId) => {
+              fieldSelection.onChange(selectedItemId)
+            }}
+          />
+        )}
+      />
+
+      <div className="absolute mt-2">
+        <FormError message={validationErrorMessage} />
+      </div>
+    </div>
   )
 }
--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,18 +1,21 @@
 import { z } from 'zod'
 
+export const generalFormErrorKey = 'general-form-error-key'
+
 export const addressSchema = z.string().min(1).max(128)
+const donationProviderIdSchema = z.string().min(1, 'Please select a provider.')
 
 export const schemaUpdateDonationLink = z.object({
   donationLinksToUpdate: z.array(
     z
       .object({
         donationLinkId: z.string().cuid(),
-        donationProviderId: z.string().min(1).optional(),
+        donationProviderId: donationProviderIdSchema.optional(),
         address: addressSchema.optional(),
       })
-      .refine(
-        (data) => !!data.address || !!data.donationProviderId,
-        'Either address or donation provider ID should be filled in.'
-      )
+      .refine((data) => !!data.address || !!data.donationProviderId, {
+        message: 'Either address or donation provider should be changed.',
+        path: [generalFormErrorKey],
+      })
   ),
 })
