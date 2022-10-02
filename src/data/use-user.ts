ff6aa9603119453435d264b3f297f3074a4abd78
Bennett Dams <dams.bennett@gmail.com>
2022-10-03 00:00:18 +0200

    feat: Sync new avatar image with "posts" cache

--- src/data/use-user.ts
+++ src/data/use-user.ts
@@ -1,6 +1,10 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { apiImageUploadAvatars } from '../services/api-service'
 import { trpc } from '../util/trpc'
+import {
+  QueryData as QueryDataPosts,
+  queryKey as queryKeyPosts,
+} from './use-posts'
 
 export function useUser(userId: string) {
   const { data, isLoading, isError } = trpc.user.byUserId.useQuery(
@@ -25,15 +29,48 @@ export function useUser(userId: string) {
 
 function useUserMutation(userId: string) {
   const utils = trpc.useContext()
+  const queryClient = useQueryClient()
 
   const updateUserImageIdMutation = useMutation(apiImageUploadAvatars, {
     onSuccess: (data) => {
       if (data.result) {
         const newImageData = data.result
+
+        // USER DATA
         utils.user.byUserId.setData(
           (prevData) =>
             !prevData ? undefined : { ...prevData, ...newImageData },
           { userId }
+        )
+
+        // USER POSTS DATA
+        utils.userPosts.byUserId.setData(
+          (prevData) =>
+            !prevData
+              ? undefined
+              : prevData.map((post) => ({
+                  ...post,
+                  author: {
+                    ...post.author,
+                    imageId: newImageData.imageId,
+                    imageBlurDataURL: newImageData.imageBlurDataURL,
+                  },
+                })),
+          { userId }
+        )
+
+        // POSTS DATA
+        queryClient.setQueryData<QueryDataPosts>(queryKeyPosts, (prevData) =>
+          !prevData
+            ? undefined
+            : prevData.map((post) => ({
+                ...post,
+                author: {
+                  ...post.author,
+                  imageId: newImageData.imageId,
+                  imageBlurDataURL: newImageData.imageBlurDataURL,
+                },
+              }))
         )
       }
     },
