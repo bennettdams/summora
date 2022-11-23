53641b8b7f4feae85e39392816b50b4bc12942b4
Bennett Dams <dams.bennett@gmail.com>
2022-11-23 15:58:24 +0100

    Add Next.js' bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   // typescript: {
   //   ignoreBuildErrors: true,
@@ -8,3 +8,20 @@ module.exports = {
     domains: ['ndzyfvfhllgysznhllnw.supabase.co'],
   },
 }
+
+module.exports = nextConfig
+
+/*
+ * Use this config instead for the script 'build:analyze'.
+ * This will create HTML files that show your bundles.
+ * Follow the CLI output for the specific location of the result.
+ */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
+// module.exports = withBundleAnalyzer(nextConfig)
+// OR
+// module.exports = (phase, defaultConfig) => {
+//   return withBundleAnalyzer(defaultConfig)
+// }
